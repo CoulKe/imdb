d0297cc3d487c5bf8a1207916055ee91ea8d1877
Luteya Coulston <thatcoul@gmail.com>
2020-12-30 19:02:30 +0300

    Completed page design

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
-const { BrowserWindow, app, Menu } = require('electron');
+const { BrowserWindow, app, Menu } = require("electron");
 
 let mainWindow;
 
 let template = [
-    {label: "Home"},
-    {label: "Help"}
+  {
+    label: "Home",
+    click() {
+      mainWindow.loadFile(`${__dirname}/index.html`);
+    },
+  },
+  {
+    label: "Help",
+    click() {
+      mainWindow.loadFile(`${__dirname}/help.html`);
+    },
+  },
 ];
 
-function createWindow(){
-    mainWindow = new BrowserWindow({
-        title: 'Movie Info',
-        width: 600,
-        height: 600,
-        webPreferences:{
-            nodeIntegration: true,
-        }
-    });
-    mainWindow.loadFile(`${__dirname}/index.html`);
-    mainWindow.webContents.openDevTools()
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    title: "Movie Info",
+    width: 600,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile(`${__dirname}/index.html`);
+  mainWindow.webContents.openDevTools();
 }
-app.whenReady().then(()=>{
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-    createWindow();
-    app.on('activate', function() {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
-app.on('window-all-closed', function() {
-    if (process.platform !== 'darwin' ) app.quit();
-})
+app.whenReady().then(() => {
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+  createWindow();
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
